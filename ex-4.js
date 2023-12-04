fb0ca4b1444e5363edb1bbc2576a38f77b112d34
Gravizz <peeke0rr@gmail.com>
2023-12-04 13:35:35 +0700

    feat(ex-3,4):complete exercise

--- ex-4.js
+++ ex-4.js
@@ -82,3 +82,14 @@ let orders = [
 ];
 
 // Start coding here
+let mostExpensive = "";
+let highestValue = 0;
+
+for (let i = 0; i < orders.length; i++) {
+  if (orders[i].productPrice > highestValue) {
+    highestValue = orders[i].productPrice;
+    mostExpensive = orders[i].productName;
+  }
+}
+
+console.log(`The most expensive product in orders: ${mostExpensive}`);
