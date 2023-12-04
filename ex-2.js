797f3fb088857cd67d9fa879aa80bace143dfd66
Gravizz <peeke0rr@gmail.com>
2023-12-04 13:12:06 +0700

    feat(ex-2):complete exercise

--- ex-2.js
+++ ex-2.js
@@ -82,3 +82,11 @@ let orders = [
 ];
 
 // Start coding here
+let jbcTotal = 0;
+for (let i = 0; i < orders.length; i++) {
+  if (orders[i].creditCardType === "jcb") {
+    jbcTotal = jbcTotal + orders[i].productPrice * orders[i].productQuantity;
+  }
+}
+let formattedJCBTotal = jbcTotal.toLocaleString();
+console.log(`Paid by JCB credit card amount: ${formattedJCBTotal} Baht`);
